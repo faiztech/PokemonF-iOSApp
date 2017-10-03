33728fb0eabc25a2bf9a65a1358b79e25596cb3c
Mohammed Faiz <mfaizudd@neiu.edu>
2017-10-03 09:42:05 -0500

    Initial Commit

---
+++ Pokemon F/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pokemon F
+//
+//  Created by Mohammed Faizuddin on 10/3/17.
+//  Copyright © 2017 Faiz Tech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      // Do any additional setup after loading the view, typically from a nib.
+   }
+
+   override func didReceiveMemoryWarning() {
+      super.didReceiveMemoryWarning()
+      // Dispose of any resources that can be recreated.
+   }
+
+
+}
+
